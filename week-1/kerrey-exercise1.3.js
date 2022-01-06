ef17cf5a33318a0395c775b843c60d457ee3bf13
skerrey <sethkerrey@gmail.com>
2022-01-06 17:41:09 -0600

    Create kerrey-exercise1.3.js

---
+++ week-1/kerrey-exercise1.3.js
@@ -0,0 +1,21 @@
+/*
+============================================
+; Title: Exercise 1.3 - Modules
+; File Name: kerrey-exercise.js
+; Author: Professor Krasso
+; Date: 9 January 2022
+; Modified By: Seth Kerrey
+; Description:
+;   Exercise to reference a module.
+; Resources:
+;   Code was provided by Prof Krasso in WEB 340/week-1/exercise1.3
+===========================================
+*/
+
+var url = require("url");
+
+var parsedURL = url.parse("https://www.example.com/profile?name=smith");
+
+console.log(parsedURL.protocol);
+console.log(parsedURL.host);
+console.log(parsedURL.query);
