57a23d88500031911759274ab55c3128acc5fb0e
skerrey <sethkerrey@gmail.com>
2022-01-28 08:31:59 -0600

    Update kerrey-exercise4.2.js

--- week-4/json-api/kerrey-exercise4.2.js
+++ week-4/json-api/kerrey-exercise4.2.js
@@ -6,16 +6,17 @@
 ; Date: 30 January 2022
 ; Modified By: Seth Kerrey
 ; Description:
-;   Exercise to understand JSON APIs with Express.
+;   Exercise to understand JSON APIs with Express and Morgan.
 ; Resources:
 ;   Code was provided by Prof Krasso in WEB 340/week-3/exercise4.2
-;   Express used
+;   Express & Morgan used
 ===========================================
 */
 
 // Require statements 
 var express = require("express"); 
 var http = require("http");
+var logger = require("morgan");
 
 var app = express(); // Placeholder for Express app
 
@@ -26,13 +27,17 @@ app.get("/customer/:id", function (req, res) {
     var id = parseInt(req.params.id, 10);
     
     res.json({
-        firstName: "Seth",
-        lastName: "Kerrey",
-        employeeId: id
+        firstName: "John",
+        lastName: "Smith",
+        email: "john@smith.com",
+        address: "123 Avenue",
+        city: "Los Angeles",
+        state: "California",
+        zip: "90017"
     });
 });
 
-// Initialize server on port 3000
-http.createServer(app).listen(3000, function() {
-    console.log("Application started on port %s", 3000);
+// Initialize server on port 8080
+http.createServer(app).listen(8080, function() {
+    console.log("Application started on port %s", 8080);
 });
