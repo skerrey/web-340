7e1a7203d109cb63f13161ae83f69162cbddae3a
skerrey <sethkerrey@gmail.com>
2022-01-28 08:36:42 -0600

    npm install morgan --save

---
+++ week-4/status-codes/kerrey-exercise4.3.js
@@ -0,0 +1,43 @@
+/*
+============================================
+; Title: Exercise 4.3 - HTTP Status Codes
+; File Name: kerrey-exercise4.2.js
+; Author: Professor Krasso
+; Date: 30 January 2022
+; Modified By: Seth Kerrey
+; Description:
+;   Exercise to understand HTTP status codes with Express and Morgan.
+; Resources:
+;   Code was provided by Prof Krasso in WEB 340/week-3/exercise4.3
+;   Express & Morgan used
+===========================================
+*/
+
+// Require statements 
+var express = require("express"); 
+var http = require("http");
+var logger = require("morgan");
+
+var app = express(); // Placeholder for Express app
+
+app.use(logger("dev")); // Logger
+
+// Return & response
+app.get("/customer/:id", function (req, res) {
+    var id = parseInt(req.params.id, 10);
+    
+    res.json({
+        firstName: "John",
+        lastName: "Smith",
+        email: "john@smith.com",
+        address: "123 Avenue",
+        city: "Los Angeles",
+        state: "California",
+        zip: "90017"
+    });
+});
+
+// Initialize server on port 8080
+http.createServer(app).listen(8080, function() {
+    console.log("Application started on port %s", 8080);
+});
