4af072cf10a9d6dc495162326bd64152ee21ef81
skerrey <sethkerrey@gmail.com>
2022-02-03 11:45:23 -0600

    Update kerrey-exercise5.2.js

--- week-5/if-else-render/kerrey-exercise5.2.js
+++ week-5/if-else-render/kerrey-exercise5.2.js
@@ -1,6 +1,6 @@
 /*
 ============================================
-; Title: Exercise 5.2 - EJS Templates
+; Title: Exercise 5.2 - if-else-render
 ; File Name: kerrey-exercise5.2.js
 ; Author: Professor Krasso
 ; Date: 6 February 2022
@@ -9,6 +9,35 @@
 ;   Exercise to understand EJS Templates with Express and EJS.
 ; Resources:
 ;   Code was provided by Prof Krasso in WEB 340/week-5/exercise5.2
-;   Express & EJSn used
+;   Express & EJS used
 ===========================================
 */
+
+// Require statements 
+var express = require("express"); 
+var http = require("http");
+var path = require("path");
+
+// App functions
+var app = express(); // Placeholder for Express app
+app.set("views", path.resolve(__dirname, "views"));
+app.set("view engine", "ejs");
+
+// Local composer array
+var composers = [
+    "Bach",
+    "Mozart",
+    "Beethoven",
+    "Verdi"
+];
+
+app.get("/", function(req, res) { // Get request
+  res.render("index", {
+      names: composers
+  })
+});
+
+// Initialize server
+http.createServer(app).listen(8080, function() {
+    console.log("Application started on port %s", 8080);
+});
