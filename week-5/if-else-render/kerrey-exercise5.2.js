b4b3688199e8c0447ba79cf348a7354988015e94
skerrey <sethkerrey@gmail.com>
2022-02-03 11:30:29 -0600

    Create kerrey-exercise5.2.js

---
+++ week-5/if-else-render/kerrey-exercise5.2.js
@@ -0,0 +1,14 @@
+/*
+============================================
+; Title: Exercise 5.2 - EJS Templates
+; File Name: kerrey-exercise5.2.js
+; Author: Professor Krasso
+; Date: 6 February 2022
+; Modified By: Seth Kerrey
+; Description:
+;   Exercise to understand EJS Templates with Express and EJS.
+; Resources:
+;   Code was provided by Prof Krasso in WEB 340/week-5/exercise5.2
+;   Express & EJSn used
+===========================================
+*/
