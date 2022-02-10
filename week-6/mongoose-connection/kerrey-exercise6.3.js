95683c7788b2bd5866806ec5f9b784a754b97279
skerrey <sethkerrey@gmail.com>
2022-02-10 11:34:20 -0600

    Update kerrey-exercise6.3.js

--- week-6/mongoose-connection/kerrey-exercise6.3.js
+++ week-6/mongoose-connection/kerrey-exercise6.3.js
@@ -12,3 +12,30 @@
 ;   Express, Morgan, & Mongoose used
 ===========================================
 */
+
+// Require statements 
+var express = require("express"); 
+var http = require("http");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+// Database connection
+var mongoDB = "mongodb+srv://admin:admin@buwebdev-cluster-1.ixkw5.mongodb.net/test";
+mongoose.connect(mongoDB);
+
+// Mongoose Promise
+mongoose.Promise = global.Promise;
+var db = mongoose.connection; // Database variable to hold connection
+db.on("error", console.error.bind(console, "MongoDB connection error: ")); // Error handling
+db.once("open", function() {
+    console.log("application connected to mLab"); // Connected message
+})
+
+// App functions
+var app = express(); // Placeholder for Express app
+app.use(logger("dev")); // Logger
+
+// Initialize server
+http.createServer(app).listen(8080, function() {
+    console.log("Application started on port %s", 8080);
+});
