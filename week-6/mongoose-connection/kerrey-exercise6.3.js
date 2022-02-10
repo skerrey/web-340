be5c9d1bd3474054a923a5cff49d96a2c4cb9ecf
skerrey <sethkerrey@gmail.com>
2022-02-10 11:19:42 -0600

    Create kerrey-exercise6.3.js

---
+++ week-6/mongoose-connection/kerrey-exercise6.3.js
@@ -0,0 +1,14 @@
+/*
+============================================
+; Title: Exercise 6.3 - Mongoose Connection
+; File Name: kerrey-exercise6.3.js
+; Author: Professor Krasso
+; Date: 13 February 2022
+; Modified By: Seth Kerrey
+; Description:
+;   Exercise to understand Mongoose.
+; Resources:
+;   Code was provided by Prof Krasso in WEB 340/week-5/exercise6.3
+;   Mongoose Used
+===========================================
+*/
