67da76ecd41169fdf9ef01467d216a5c42311077
skerrey <sethkerrey@gmail.com>
2022-02-15 18:53:49 -0600

    Update app.js

--- ems/app.js
+++ ems/app.js
@@ -13,18 +13,22 @@
 ===========================================
 */
 
-// Require statements 
+// Require statements ---------------------------------------------------------
 var express = require("express");
 var http = require("http");
 var path = require("path");
 var logger = require("morgan");
+var mongoose = require("mongoose");
+const employee = require("./modes/employee");
 
-// App functions
+// App functions --------------------------------------------------------------
 var app = express(); // Placeholder for Express app
-app.set("views", path.resolve(__dirname, "views"));
+app.set("views", path.resolve(__dirname, "views")); // Directory 
 app.set("view engine", "ejs"); // Views
 app.use(logger("short")); // Logger
 
+
+// Pages: home, new, list, view -------------------------------------------------
 app.get("/", function (req, res) { // index.ejs
     res.render("index", {
         title: "Home page"
@@ -49,9 +53,26 @@ app.get("/view", function (req, res) { // view.ejs
     });
 });
 
+// mLab connection string, Mongoose connection code, employee model ---------------
+// Database connection
+var mongoDB = "mongodb+srv://admin:admin@buwebdev-cluster-1.ixkw5.mongodb.net/test"; 
+mongoose.connect(mongoDB);
 
+// Mongoose Promise
+mongoose.Promise = global.Promise;
+var db = mongoose.connection; // Database variable to hold connection
+db.on("error", console.error.bind(console, "MongoDB connection error: ")); // Error handling
+db.once("open", function() {
+    console.log("application connected to mLab"); // Connected message
+})
 
-// Initialize server
+// Model
+var employee = new employee({
+    firstName: "Leroy",
+    lastName: "Jenkins"
+})
+
+// Initialize server -------------------------------------------------------------
 http.createServer(app).listen(8080, function() {
     console.log("Application started on port %s", 8080);
 });
