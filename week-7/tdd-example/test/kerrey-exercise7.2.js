0c5e039e4ae6a63874c16c6630bdf5bcfb3718e7
skerrey <sethkerrey@gmail.com>
2022-02-15 18:06:37 -0600

    Create kerrey-exercise7.2.js

---
+++ week-7/tdd-example/test/kerrey-exercise7.2.js
@@ -0,0 +1,28 @@
+/*
+============================================
+; Title: Exercise 7.2 - TDD in Action
+; File Name: kerrey-exercise7.2.js
+; Author: Professor Krasso
+; Date: 13 February 2022
+; Modified By: Seth Kerrey
+; Description:
+;   Exercise to understand TDD in action.
+; Resources:
+;   Code was provided by Prof Krasso in WEB 340/week-7/exercise7.2
+;   Mocha and Chai used
+===========================================
+*/
+
+var assert = require("assert");
+describe("String#split", function() {
+    it("should return an array of fruits", function() {
+        assert(Array.isArray("Apple,Orange,Mango".split(",")));
+    });
+});
+
+
+function getFruits(str) {
+    return str.split(",");
+}
+
+module.exports = getFruits;
